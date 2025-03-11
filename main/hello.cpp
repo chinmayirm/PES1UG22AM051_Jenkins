1ef56dfe3f5e384dc8165b95de339f8404d9c8c9
chinmayirm <121074867+chinmayirm@users.noreply.github.com>
2025-03-11 16:24:25 +0530

    Add files via upload

---
+++ main/hello.cpp
@@ -0,0 +1,12 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  std::cout << "Hello, PES1IG22AM051!" << std::endl;
+  std::cout << "PES1UG22AM051" << std::endl; // Added print statement
+  return 0;
+}
